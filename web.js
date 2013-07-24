4c50b658c85f7d46bb80732ebde063f940d4ca5f
Juan Sebastian Galarza <jg@linkofertas.com>
2013-07-24 17:33:17 -0500

    return buf to string web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var buf = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(buf);
+  response.send(buf.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
