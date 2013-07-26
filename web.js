e4c5b868a52798bf6ee7748e3ba447151da937b0
root <root@ip-10-112-37-173.ec2.internal>
2013-07-26 22:34:33 +0000

    primera version del sitio con estilos

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
-
 var fs = require('fs');
-var buf = fs.readFileSync('index.html');
+var htmlfile = 'index.html';
 
+var app = express();
+app.use(express.static(__dirname + '/assets'));
 app.get('/', function(request, response) {
-  response.send(buf.toString("utf-8"));
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
